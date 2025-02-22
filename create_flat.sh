320c1e7b9516b0a1534a50fdff6932ed1eaa4f71
ReyoKatsu <honekatsu@github.com>
2025-02-23 01:32:31 +0900

    add flatcar_loki

--- create_flat.sh
+++
@@ -1,4 +0,0 @@
-echo "pwd: ${PWD}"
-set -e
-cat flat_base.yaml | docker run -i --rm -v ${PWD}:/pwd quay.io/coreos/butane:release --files-dir /pwd > flat_base.json
-cat flat.yaml | docker run -i --rm -v ${PWD}:/pwd quay.io/coreos/butane:release --files-dir /pwd > flat.json
