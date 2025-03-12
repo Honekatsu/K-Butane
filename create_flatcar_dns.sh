0ba88543f77772b4ed6f66b51ccceb4e3f8d148d
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2025-03-13 00:19:56 +0900

    add flatcar_dns02

--- create_flatcar_dns.sh
+++ create_flatcar_dns.sh
@@ -3,4 +3,6 @@ set -e
 
 echo "pwd: ${PWD}"
 docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_base/flatcar_base.yaml > out/flatcar_base.json
+docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns_base.yaml > out/flatcar_dns_base.json
 docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns01.yaml > out/flatcar_dns01.json
+docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns02.yaml > out/flatcar_dns02.json
