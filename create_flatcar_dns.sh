7f4b67869c46569c0513af0529ddab223617b77f
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2025-03-16 20:52:59 +0900

    flatcar_dns to flatcar_net

--- create_flatcar_dns.sh
+++ create_flatcar_dns.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "pwd: ${PWD}"
 docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_base/flatcar_base.yaml > out/flatcar_base.json
-docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns_base.yaml > out/flatcar_dns_base.json
-docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns01.yaml > out/flatcar_dns01.json
-docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_dns/flatcar_dns02.yaml > out/flatcar_dns02.json
+docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_net/flatcar_net_base.yaml > out/flatcar_net_base.json
+docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_net/flatcar_net01.yaml > out/flatcar_net01.json
+docker run -i --rm -v "${PWD}":/pwd quay.io/coreos/butane:release --files-dir /pwd < config/flatcar_net/flatcar_net02.yaml > out/flatcar_net02.json
